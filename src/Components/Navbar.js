1a2d3c4686c10a54521445d47e17059a00f19d48
lucasgastaldi <72451454+lucasgastaldi@users.noreply.github.com>
2021-04-13 15:50:24 -0300

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function nav() {
+    return (
+        <div className="App">
+            <nav>
+                <ul className="App-link">
+                    <li><a>Home</a></li>
+                    <li><a>Productos</a></li>
+                    <li><a>Historia</a></li>
+                    <li><a>Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default nav;
