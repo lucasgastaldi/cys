b3db5c1b897348c185929a0dcbde44221dad2063
lucasgastaldi <72451454+lucasgastaldi@users.noreply.github.com>
2021-04-26 15:24:54 -0300

    Add files via upload

---
+++ src/Components/ItemCount/ItemCount.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'  
+import carrito from '../Cart/carrito.png';
+
+export function ItemCount(props) {   
+    const [count, setCount] = useState (props.initial)
+
+    const RestarItem = () => props.initial < count && setCount(count - 1)   
+    
+    const SumarItem = () => count < props.stock && setCount(count + 1)    
+
+    return (
+        <div>
+            <h5 className="subtitulo">Ingrese la cantidad de productos que desea:</h5>
+            <div className='itemCount'>
+                <button className='btn-floating btn-large cyan pulse' onClick={RestarItem}>-</button>
+                <div className="contador">
+                    <p>{count}</p>
+                </div>
+                <button className='btn-floating btn-large cyan pulse' onClick={SumarItem}>+</button>
+            </div>           
+            <p className="subtitulo__1">Stock: {props.stock}</p>
+            <div className="agregarAlCarrito">
+                <button className="waves-effect waves-teal btn-flat"><img src={carrito} height="40px" alt="carrito" /></button>
+            </div>      
+        </div> 
+    )
+}
--- src/Components/Items/ItemListContainer.js
+++ src/Components/Items/ItemListContainer.js
@@ -1,7 +1,10 @@
+import { ItemCount } from '../ItemCount/ItemCount'
+
 export const ItemListContainer = (props) => {
     return (
         <div>
             <h3 className="saludo">{props.greeting}</h3>
+            <ItemCount initial = {1} stock="7" />
         </div>
     )
 }
