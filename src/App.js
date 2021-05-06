c92271cdb290048fdb171a1f1dc0f8b1cfaab51b
lucasgastaldi <72451454+lucasgastaldi@users.noreply.github.com>
2021-05-05 22:15:48 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import { ItemListContainer } from './Components/ItemListContainer/ItemListContainer';
-import { NavBar } from './Components/NavBar/Navbar'
+import { NavBar } from './Components/Nav/Navbar'
+import { ItemDetailContainer } from './Components/ItemDetailContainer/ItemDetailContainer'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   const greeting = "Bienvenido a CyS!"
     return (
-      <div className="App">
-        <NavBar />
-        <ItemListContainer greeting={greeting} />
-      </div>
+      <BrowserRouter>
+      <NavBar />
+        <Switch>
+          <Route exact path='/'>
+            <div className="App">   
+              <ItemListContainer greeting={greeting} />
+            </div>
+          </Route>
+          <Route exact path='/productos'>
+            <ItemListContainer />
+          </Route>
+          <Route exact path='/productos/:productId'>
+            <ItemDetailContainer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
   );
 }
 
