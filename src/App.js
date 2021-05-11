150fecb5e1e102dca234bfa0527f1e244fd63fd4
lucasgastaldi <72451454+lucasgastaldi@users.noreply.github.com>
2021-05-10 21:27:55 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
 import { ItemListContainer } from './Components/ItemListContainer/ItemListContainer';
-import { NavBar } from './Components/Nav/Navbar'
-import { ItemDetailContainer } from './Components/ItemDetailContainer/ItemDetailContainer'
+import { NavBar } from './Components/NavBar/Navbar';
+import { ItemDetailContainer } from './Components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Cart } from './Components/Cart/Cart'
 
 function App() {
   const greeting = "Bienvenido a CyS!"
     return (
       <BrowserRouter>
-      <NavBar />
-        <Switch>
-          <Route exact path='/'>
-            <div className="App">   
-              <ItemListContainer greeting={greeting} />
-            </div>
-          </Route>
-          <Route exact path='/productos'>
-            <ItemListContainer />
-          </Route>
-          <Route exact path='/productos/:productId'>
-            <ItemDetailContainer />
-          </Route>
-        </Switch>
+        <NavBar />
+          <Switch>
+            <Route exact path='/'>
+              <div className="App">   
+                <ItemListContainer greeting={greeting} />
+              </div>
+            </Route>
+            <Route exact path='/productos'>
+              <ItemListContainer />
+            </Route>
+            <Route exact path='/productos/:productId'>
+              <ItemDetailContainer />
+            </Route>
+            <Route exact path='/cart'>
+              <Cart />
+            </Route>
+          </Switch>
       </BrowserRouter>
   );
 }
